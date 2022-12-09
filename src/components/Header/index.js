2d81621ff2675e1b3be7067928df211c79e4b67d
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-12-09 19:58:07 -0300

    admin page customization

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import './header.css'
+import { BiLogOut } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../services/firebaseConnection'
+
+export function Header(){
+
+  async function handleLogout(){
+    await signOut(auth)
+  }
+
+  return(
+    <header className="admin-header">
+      <nav className="nav-header">
+        <button onClick={handleLogout}>
+          <BiLogOut size={28} color="#DB2629" />
+        </button>
+
+          <Link to="/admin">
+          Links
+        </Link>
+          <Link to="/admin/social">
+          Redes Sociais
+        </Link>
+      </nav>
+    </header>
+  )
+}
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,7 +1,88 @@
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/input";
+import { Logo } from "../../components/Logo";
+import { MdAddLink } from "react-icons/md"
+import { FiTrash2 } from "react-icons/fi"
+
+import './admin.css'
+
+
+
 export default function Admin(){
+  const [nameInput, setNameInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+  const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1");
+  const [textColorInput, setTextColorInput] = useState("#121212");
+
   return(
-    <div>
-      <h1>Pagina do Administrador</h1>
+    <div className="admin-container">
+      <Header />
+      <Logo/>
+      <form className="form">
+        <label className="label">Nome do Link</label>
+        <Input 
+          placeholder="Nome do seu link..."
+          value={nameInput}
+          onChange={ (e) => setNameInput(e.target.value) }  
+          />
+        <label className="label">Url do link</label>
+        <Input
+          type="url"
+          placeholder="Digite a url..."
+          value={urlInput}
+          onChange={ (e) => setUrlInput(e.target.value) }
+          />
+        <section className="container-colors">
+
+          <div>
+            <label className="label right">Fundo do Link</label>
+            <input
+              type="color"
+              value={backgroundColorInput}
+              onChange={ (e) => setBackgroundColorInput(e.target.value) }
+              />
+          </div>  
+
+          <div>
+            <label className="label right">Cor do Link</label>
+            <input
+              type="color"
+              value={textColorInput}
+              onChange={ (e) => setTextColorInput(e.target.value) }
+              />
+          </div>
+        </section>
+
+        { nameInput !== '' && (
+          <div className="preview">
+            <label className="label">Veja como esta ficando </label>
+            <article className="list" style={{ marginTop: 8, backgroundColor: backgroundColorInput }}>
+              <p style={{ color: textColorInput }} >{nameInput}</p>
+            </article>
+          </div>
+        )}
+
+        <button className="btn-register" type="submit">
+          Cadastrar <MdAddLink size={24} color="#FFF" />
+        </button>
+      </form>
+
+      <h2 className="title">
+        Meus Links
+      </h2>
+
+      <article 
+        className="list animate-pop"
+        style={{ backgroundColor: "#000", color: "#FFF" }}
+      >
+        <p>Grupo Exclusivo do Telegram</p>
+        <div>
+          <button className="btn-delete">
+            <FiTrash2 size={18} color="#FFF" />
+          </button>
+        </div>
+      </article>
     </div>
   )
 }
